c1b887f00b9e7dc75e48ea3db11011031722d0c9
Aleksandr <sirenko.alexandr.l@gmail.com>
2017-08-24 06:23:39 +0000

    added form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(function(){
-	
+	$('.collapse').on('show.bs.collapse', function(){
+	    $(this).prev().find('.fa').removeClass().addClass('fa fa-minus-circle');
+	});
+	$('.collapse').on('hide.bs.collapse', function(){
+	    $(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
+	});
 });
