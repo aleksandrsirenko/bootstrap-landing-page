db8cf0ea971ec601c046233219be9db166dfc67a
Aleksandr <sirenko.alexandr.l@gmail.com>
2017-08-26 18:52:18 +0000

    added footer

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,19 @@ $(function(){
 	$('.collapse').on('hide.bs.collapse', function(){
 	    $(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
 	});
+	
+	$('#myForm').validator({
+		feedback: {
+		success: 'fa fa-check-circle',
+		error: 'fa fa-times-circle'
+		}
+	});
+	
+	$('.form-sel').select2({
+		minimumResultsForSearch: Infinity
+	});
+	
+	new WOW({
+		offset: 100
+	}).init();
 });
